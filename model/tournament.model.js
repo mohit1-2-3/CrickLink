4b7ff0741c4309b68059b99726b0e2669ccdc2ea
Kirti <kirtijhala1110@gmail.com>
2025-01-07 10:07:30 +0530

    new changes made

--- model/tournament.model.js
+++ model/tournament.model.js
@@ -1,58 +1,39 @@
 import mongoose from "mongoose";
-const tournamentSchema  = new mongoose.Schema({
-    TournamentName: {
-      type: String,
-      required: true,
-    },
-    organizerId: {
+const tournamentSchema = new mongoose.Schema({
+  TournamentName: {
+    type: String,
+    required: true,
+  },
+  organizerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  teams: [
+    {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User', 
-      required: true,
+      ref: 'Team',
     },
-    teams: [
-      {
+  ],
+  schedule: [
+    {
+      matchId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Team', 
+        ref: 'Match',
+        required: true,
       },
-    ],
-    schedule: [
-      {
-        matchId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: 'Match',
-          required: true,
-        },
-        team1: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: 'Team', 
-          required: true,
-        },
-        team2: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: 'Team', 
-          required: true,
-        },
-        date: {
-          type: Date,
-          required: true,
-        },
-        venue: {
-          type: String,
-          required: true,
-        },
-      },
-    ],
-    startDate: {
-      type: Date,
-      required: true,
-    },
-    endDate: {
-      type: Date,
-      required: true,
-    },
-  });
-  
-  
-  export const Tournament = mongoose.model('Tournament', tournamentSchema);
-  
-  
+    }
+  ],
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+
+export const Tournament = mongoose.model('Tournament', tournamentSchema);
+
