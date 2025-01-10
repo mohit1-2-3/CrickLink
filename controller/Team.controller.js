bd83925f5c1dd5842bfa91fb1e2789f69a559596
Jyoti Patewar <jyotipatewar2004@gmail.com>
2025-01-10 10:26:40 +0530

    add file

--- controller/Team.controller.js
+++ controller/Team.controller.js
@@ -1,21 +1,5 @@
 
-import { validationResult } from "express-validator"
-import Team from "../model/team.model";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 
-
-export const createTeam= async (req, res) => {
-  const { teamName, captainId } = req.body;
-
-  try {
-    const newTeam = new Team({ teamName, captainId });
-    await newTeam.save();
-    res.status(201).json({ message: "Team created successfully", team: newTeam });
-  } catch (error) {
-    res.status(500).json({ message: "Error creating team", error: error.message });
-  }
-};
 
 
 //==============================================
